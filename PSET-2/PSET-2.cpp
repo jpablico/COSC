2ec8f894484882313b6ffc6d101c7122c4eb1adb
Josh Pablico <jbpablico27@gmail.com>
2023-08-31 13:50:12 -0500

    Finished PSET 2

--- PSET-2/PSET-2.cpp
+++ PSET-2/PSET-2.cpp
@@ -4,8 +4,23 @@ using namespace std;
 
 int main() 
 {
-	double mealCost;
-	const double tax = 0.0675;
-
-	return 0;
+    
+    double meal;
+    double total;
+    double tax = 0.0675;
+    double tip = 0.2;
+    
+    std::cout << "Enter meal cost: ";
+    std::cin >> meal;
+    
+    double taxed = meal * tax;
+    double tipped = taxed * tip;
+    total = meal + taxed + tipped;
+	
+    std::cout.precision(3);
+    std::cout << "Meal cost: " << meal <<std::endl;
+    std::cout << "Taxed amount: " << taxed <<std::endl;
+    std::cout << "Tipped amount: " << tipped <<std::endl;
+    std::cout << "Total amount: " << total <<std::endl;
+    return 0;
 }
