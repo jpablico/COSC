2cb1ea6a6791184197817b45f3328177507c5fbe
Josh Pablico <jbpablico27@gmail.com>
2023-09-12 14:37:03 -0500

    PSET 4

--- PSET-4/PSET-4.cpp
+++ PSET-4/PSET-4.cpp
@@ -5,32 +5,23 @@
 using namespace std;
 
 int main() {
-
 	double loanAmount;
-	double annualPercentageRate;
-	double durationOfLoan;
+	double interestRate;
+	double numberOfPayments;
 
-	double durationOfLoanYear = durationOfLoan / 12;
-	double rate = (annualPercentageRate / 100) / 12;
-
-	double b = (1 + rate);
-	double e = (double) durationOfLoan;
-
-	double pow = (double b, double e);
-
-	double Payment;
-
-	cout.precision(2);
-	cout << "Loan amount: $";
+	cout << "Loan Amount: ";
 	cin >> loanAmount;
-	cout << "\n" << "Annual Interest Rate: ";
-	cin >> annualPercentageRate; 
-	cout << "% \n";
-	cout << "Number of Payments: ";
-	cin >> durationOfLoan;
-	cout << "\n Monthly Payment: $";
-	
+	cin >> interestRate;
+	cin >> numberOfPayments;
 
-
+	double interestRateMonthly = (interestRate / 100.0) / 12;
+	double onePlusRate = pow(1 + interestRateMonthly, numberOfPayments);
+	double payment = loanAmount * (( interestRateMonthly * onePlusRate ) / (onePlusRate - 1));
+	double paymentLoan = payment * numberOfPayments;
+	cout << setprecision(2) << fixed;
+	cout << "Interest rate: "<< interestRateMonthly << endl;
+	cout << "1+r "<<onePlusRate << endl;
+	cout << "Payment " <<  payment << endl;
+	cout << "Total " << paymentLoan;
 	return 0;
 }
