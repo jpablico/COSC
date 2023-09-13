5d988d71aabf15e697233e90b8dc91cebda29d16
Josh Pablico <jbpablico27@gmail.com>
2023-09-13 10:28:49 -0500

    PSET-4 finished

--- PSET-4/PSET-4.cpp
+++ PSET-4/PSET-4.cpp
@@ -11,17 +11,21 @@ int main() {
 
 	cout << "Loan Amount: ";
 	cin >> loanAmount;
+	cout << "Annual Interest Rate: ";
 	cin >> interestRate;
+	cout << "Number of Payments: ";
 	cin >> numberOfPayments;
 
 	double interestRateMonthly = (interestRate / 100.0) / 12;
 	double onePlusRate = pow(1 + interestRateMonthly, numberOfPayments);
 	double payment = loanAmount * (( interestRateMonthly * onePlusRate ) / (onePlusRate - 1));
 	double paymentLoan = payment * numberOfPayments;
+	double interestPaid = paymentLoan - loanAmount;
+
 	cout << setprecision(2) << fixed;
-	cout << "Interest rate: "<< interestRateMonthly << endl;
-	cout << "1+r "<<onePlusRate << endl;
-	cout << "Payment " <<  payment << endl;
-	cout << "Total " << paymentLoan;
+	cout << "Monthly Payment: $" << payment << endl;
+	cout << "  Amount Paid Back: $" << paymentLoan << endl;
+	cout << "  Interest Paid: $" << interestPaid << endl;
+
 	return 0;
 }
