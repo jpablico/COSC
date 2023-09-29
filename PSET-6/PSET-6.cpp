b2977177f93cbc13b1c98f5bd09b9f3cb89e1459
Josh Pablico <jbpablico27@gmail.com>
2023-09-29 18:38:40 -0500

    Looping|add fix

--- PSET-6/PSET-6.cpp
+++ PSET-6/PSET-6.cpp
@@ -11,21 +11,56 @@ int main() {
 	const double helium = 972.0;
 	const double hydrogen = 1270.0;
 
-	string selection;
+	int selection;
+	double time;
+	double meters;
 
 	cout << "Menu of Gases" << endl;
-	cout << "A. Carbon Dioxide" << endl;
-	cout << "B. Air" << endl;
-	cout << "C. Heliulm" << endl;
-	cout << "D. Hydrogen" << endl;
+		cout << "1. Carbon Dioxide" << endl;
+		cout << "2. Air" << endl;
+		cout << "3. Heliulm" << endl;
+		cout << "4. Hydrogen" << endl;
+		cout << "5. End program" << endl;
 
-	cout << "\nPlease choose one of the gases: ";
-	cin >> selection;
-	cout << endl;
+		cout << "\nPlease choose one of the gases: ";
+		cin >> selection;
+		cout << endl;
+	while(selection != 1 && 2 &&  3 && 4 && 5) {
+		cout << "Menu of Gases" << endl;
+		cout << "1. Carbon Dioxide" << endl;
+		cout << "2. Air" << endl;
+		cout << "3. Heliulm" << endl;
+		cout << "4. Hydrogen" << endl;
+		cout << "5. End program" << endl;
 
-	switch(selection) {
-		case "A":  
-			break;
+		cout << "\nPlease choose one of the gases: ";
+		cin >> selection;
+		cout << endl;
 	}
+		cout << "Please indicate the time in seconds for the sound to travel in this medium: ";
+		cin >> time;
+
+			switch (selection) {
+				case 1:  
+						meters = time * carbonDioxide;
+						cout << "Sound traveled " << meters << " meters from the source in " << time << " seconds." << endl;
+					break;
+				case 2:  
+						meters = time * air;;
+						cout << "Sound traveled " << meters << " meters from the source in " << time << " seconds." << endl;
+					break;
+				case 3:  
+						meters = time * helium;
+						cout << "Sound traveled " << meters << " meters from the source in " << time << " seconds." << endl;
+					break;
+				case 4:  
+						meters = time * hydrogen;;
+						cout << "Sound traveled " << meters << " meters from the source in " << time << " seconds." << endl;
+					break;
+				case 5:
+					break;
+			}
+	
+	
 	return 0;
 }
