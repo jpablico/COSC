e4e0108d2a7dc1320ada14c160898b47388916d5
Josh Pablico <jbpablico27@gmail.com>
2023-09-28 22:30:06 -0500

    PSET-6

--- PSET-6/PSET-6.cpp
+++ PSET-6/PSET-6.cpp
@@ -6,5 +6,25 @@ using namespace std;
 
 int main() {
 
+	const double carbonDioxide = 258.0;
+	const double air = 331.5;
+	const double helium = 972.0;
+	const double hydrogen = 1270.0;
+
+	string selection;
+
+	cout << "Menu of Gases" << endl;
+	cout << "A. Carbon Dioxide" << endl;
+	cout << "B. Air" << endl;
+	cout << "C. Heliulm" << endl;
+	cout << "D. Hydrogen" << endl;
+
+	cout << "\nPlease choose one of the gases: ";
+	cin >> selection;
+	cout << endl;
+
+	switch(selection) {
+
+	}
 	return 0;
 }
