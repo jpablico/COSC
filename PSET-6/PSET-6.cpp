89edea6df83e8dc73ded36315fc2bd9b3c565c95
Josh Pablico <jbpablico27@gmail.com>
2023-09-29 20:42:32 -0500

    PSET 6 cmp

--- PSET-6/PSET-6.cpp
+++ PSET-6/PSET-6.cpp
@@ -16,51 +16,50 @@ int main() {
 	double meters;
 
 	cout << "Menu of Gases" << endl;
-		cout << "1. Carbon Dioxide" << endl;
-		cout << "2. Air" << endl;
-		cout << "3. Heliulm" << endl;
-		cout << "4. Hydrogen" << endl;
-		cout << "5. End program" << endl;
+	cout << "1. Carbon Dioxide" << endl;
+	cout << "2. Air" << endl;
+	cout << "3. Heliulm" << endl;
+	cout << "4. Hydrogen" << endl;
+	cout << "5. End program" << endl;
 
-		cout << "\nPlease choose one of the gases: ";
-		cin >> selection;
-		cout << endl;
-	while(selection != 1 && 2 &&  3 && 4 && 5) {
-		cout << "Menu of Gases" << endl;
-		cout << "1. Carbon Dioxide" << endl;
-		cout << "2. Air" << endl;
-		cout << "3. Heliulm" << endl;
-		cout << "4. Hydrogen" << endl;
-		cout << "5. End program" << endl;
-
-		cout << "\nPlease choose one of the gases: ";
-		cin >> selection;
-		cout << endl;
-	}
+	cout << "\nPlease choose one of the gases: ";
+	cin >> selection;
+	cout << endl;
+	if(selection <= 0) {
+		cout << "Please enter a valid option." << endl;
+	} else if(selection >= 1 && selection <= 4) {
 		cout << "Please indicate the time in seconds for the sound to travel in this medium: ";
 		cin >> time;
 
+		if(time <= 0) {
+			cout << "Time is invalide." << endl;
+		} 
+		else if(time >= 1 && time <=30) {
 			switch (selection) {
 				case 1:  
 						meters = time * carbonDioxide;
-						cout << "Sound traveled " << meters << " meters from the source in " << time << " seconds." << endl;
+						cout << "Within a medium of carbon dioxide, sound traveled " << meters << " meters from the source in " << time << " seconds." << endl;
 					break;
 				case 2:  
-						meters = time * air;;
-						cout << "Sound traveled " << meters << " meters from the source in " << time << " seconds." << endl;
+						meters = time * air;
+						cout << "Within a medium of air, sound traveled " << meters << " meters from the source in " << time << " seconds." << endl;
 					break;
 				case 3:  
 						meters = time * helium;
-						cout << "Sound traveled " << meters << " meters from the source in " << time << " seconds." << endl;
+						cout << "Within a medium of helium, sound traveled " << meters << " meters from the source in " << time << " seconds." << endl;
 					break;
 				case 4:  
 						meters = time * hydrogen;;
-						cout << "Sound traveled " << meters << " meters from the source in " << time << " seconds." << endl;
-					break;
-				case 5:
+						cout << "Within a medium of hydrogen, sound traveled " << meters << " meters from the source in " << time << " seconds." << endl;
 					break;
 			}
-	
-	
+		} else {
+			cout << "Time is invalide." << endl;
+		}
+	} else if(selection == 5) {
+		return 0;
+	} else {
+		cout << "Please enter a valid option." << endl;
+	}
 	return 0;
 }
