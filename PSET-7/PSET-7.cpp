2d9ea07426c286f7b683e1c7349868206b403ce0
Josh Pablico <jbpablico27@gmail.com>
2023-10-04 10:54:55 -0500

    PSET-7 completed

--- PSET-7/PSET-7.cpp
+++ PSET-7/PSET-7.cpp
@@ -6,25 +6,38 @@
 using namespace std;
 
 int main() {
-
-	string gameStart = "Y";
+	
 	int guessNumber;
+	int numberGuesses = 1;
+	string gameStart;
+	 do {
+		srand(time(0));
+		int randNumber = rand() % 100 + 1;
 
-	srand(time(0));
-	int randNumber = rand() % 100 + 1;
-
-	if(gameStart == "Y") {
-		cout << "I'm thinking of a number. Can you guess what it is? " << endl;
+		cout << "I'm thinking of a number. Can you guess what it is? ";
 		cin >> guessNumber;
 
-		while(guessNumber != randNumber) {
-			if(guessNumber > randNumber){
-				cout << "Sorry, that guess is too high." << 
+		while (guessNumber != randNumber) {
+			if (guessNumber > randNumber) {
+				cout << "Sorry, that guess is too high" << endl;
+				cout << "Guess again: ";
+				cin >> guessNumber;
+				numberGuesses++;
+			} else if (guessNumber < randNumber) {
+				cout << "Sorry, that guess is too low" << endl;
+				cout << "Guess again: ";
+				cin >> guessNumber;
+				numberGuesses++;
 			}
-		} return 
-	}
-
-	cout << randNumber << endl;
-	cout << randNumber;
+		}
+		cout << endl;
+		cout << "Congratulations! You guessed it!\nI was thinking of the number " << randNumber << "." << endl;
+		cout << "You got it right in " << numberGuesses << " guesses.";
+		cout << endl;
+		cout << "Would you like to play another game? (Y or N): ";
+		numberGuesses = 1;
+		cin >> gameStart;
+	} while (gameStart != "N");
+	cout << "End of game.";
 	return 0;
 }
