36d10d89ec0e146b6c6dca9e2f52a1a0dbdcda1e
Josh Pablico <jbpablico27@gmail.com>
2023-10-13 07:49:51 -0500

    PSET-8

--- PSET-8/PSET-8.cpp
+++ PSET-8/PSET-8.cpp
@@ -1,26 +1,45 @@
 #include <iostream>
-#include <iomanip>
 #include <cstdlib>
+#include <ctime>
 #include <cmath>
 
 using namespace std;
 
 int main() {
 
+	int gameCount = 0;
 	int numberOfGames;
+	int playerWins = 0;
+	int houseWins = 0;
+	int gamePoint;
+	int diceRollCount = 0;
+
+	int seed;
+	srand(seed);
 
 	cout << "How many games do you wish to play? ";
 	cin >> numberOfGames;
 	cout << endl;
 
+	while (gameCount != numberOfGames) {
+		int diceRoll = (rand() % 6) + (rand() % 6) + 2;
+		diceRollCount++;
 
+		if (diceRoll == 7 || diceRoll == 11) {
+			playerWins++;
+		} else if (diceRoll == 2 || diceRoll == 3 || diceRoll == 12) {
+			
+		}
+	}
 	
+
+
+
 	for (int i = 0; i <= numberOfGames; i++) {
 		int gameValue = 1;
 
 		int playerWins = 0;
-		int houseWins = 0;
-		int gamePoint;
+		
 
 
 		srand(time(0));
