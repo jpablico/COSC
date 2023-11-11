640c7731f1e613fab2af6132894a722c54b0ea37
Josh Pablico <jbpablico27@gmail.com>
2023-11-11 16:50:47 -0600

    1 and 2 parts of assignment completed.

--- PSET-11/PSET-11.cpp
+++ PSET-11/PSET-11.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <cstdlib>
 #include <cmath>
 #include <iomanip>
+#include <ctime>
 
 using namespace std;
 
 int testArray[20][10];
+
+int main() {
+	srand((unsigned)time(0));
+	int randInt;
+
+	for (int i = 0; i < 20; i++) {
+		for (int j = 0; j < 10; j++) {
+			randInt = (rand() % 45) + 55;
+			testArray[i][j] = randInt;
+		}
+	}
+
+	int n = sizeof(testArray)/sizeof(testArray[0]);
+	for (int i = 0; i < 19; i++) {
+		sort(testArray[i], testArray[i] + n);
+	}
+
+	for (int i = 0; i < 20; i++) {
+		for (int j = 0; j < 10; j++)
+			cout << setw(3) << testArray[i][j] << " ";
+		cout << endl;
+	}
+
+
+	
+	return 0;
+}
