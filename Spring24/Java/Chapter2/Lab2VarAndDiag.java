51b1d1b2542305c800f7ab3a80146c473c343d22
Josh Pablico <jbpablico27@gmail.com>
2024-01-23 09:41:45 -0600

    First Java

---
+++ Spring24/Java/Chapter2/Lab2VarAndDiag.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+	/*
+	 * This program demonstrates the use of variables and dialog boxes with JOptionPane.
+	 * Author: Josh Pablico
+	 */
+
+public class Lab2VarAndDiag {
+
+	public static void main(Strings[] args) {
+		
+	}
+}
