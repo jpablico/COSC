8c0353d1363e34113776d6b2959df9ef021e5581
Josh Pablico <jbpablico27@gmail.com>
2024-01-25 10:08:09 -0600

    Labdiag

--- Spring24/Java/Chapter2/LabWork/Lab2VarAndDiag.java
+++ Spring24/Java/Chapter2/LabWork/Lab2VarAndDiag.java
@@ -12,19 +12,30 @@ public class Lab2VarAndDiag {
 	public static void main(String[] args) {
 		String name;				 // To read the user's name
 		String inputString;		// To read the user's numerical inputs
-		int  age; 					 // To hold the user's age
-		int inputTemp;			    // To hold the user's input temporarily
+		int  userlvl; 					 // To hold the user's age
+		int bossLvl;			    // To hold the user's input temporarily
 
 		// Get the User's name
 		name = JOptionPane.showInputDialog("What is a your character's name?");
 
-		// Get the User's age
-		inputString = JOptionPane.showInputDialog("What is your character's age?");
+		// Get the User's level
+		inputString = JOptionPane.showInputDialog("What is your character's level?");
 
-		// Convert inputString to int age
-		age = Integer.parseInt(inputString);
+		// Convert inputString to int and store in level
+		userlvl = Integer.parseInt(inputString);
 
+		// Get the Boss's level
+		inputString = JOptionPane.showInputDialog("What is the boss's level?");
 
+		// Convert inputString to int and store in bossLvl
+		bossLvl = Integer.parseInt(inputString);
 
+		// Mad Libs logic and results display
+		// If the user's level is greater than or equal to the boss's level, then the user can defeat the boss
+		if (userlvl >= bossLvl) {
+			JOptionPane.showMessageDialog(null, "Hello " + name + ". Your character is level " + userlvl + ". The boss is level " + bossLvl + ".\n" + "You can defeat the boss!");
+		} else {
+			JOptionPane.showMessageDialog(null, "Hello " + name + ". Your character is level " + userlvl + ". The boss is level " + bossLvl + ".\n" + "You cannot defeat the boss!");
+		} 
 	}
 }
