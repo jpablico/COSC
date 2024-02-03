50618084d7a1b40e4c848d25d417f164719bfbdc
Josh Pablico <jbpablico27@gmail.com>
2024-02-02 21:07:26 -0600

    updated all scenarios

--- Spring24/Java/Chapter3/Lab3MenuDriven/HPBattle.java
+++ Spring24/Java/Chapter3/Lab3MenuDriven/HPBattle.java
@@ -12,58 +12,67 @@ public class HPBattle {
 
 		int menuInputInt = 1;
 		while (menuInputInt != 0) {
-			System.out.println("Welcome to the Hogwarts Battle Simulator!");
-			System.out.println("Please select a monster to battle:");
-				System.out.println("1. Grindylow");
-				System.out.println("2. Troll");
-				System.out.println("3. Basilisk");
-				System.out.println("4. Dementor");
+				System.out.println("Harry walks up to the pensive.");
+				System.out.println("What memory does Harry wish to relive?");
+				System.out.println("1. Drowning in the swarm of Grindylows in the Black Lake");
+				System.out.println("2. Confronting a Troll that escaped the dungeon");
+				System.out.println("3. Battling the Basilisk inside the chamber of secrets");
+				System.out.println("4. A Dementor's kiss");
 				System.out.println("0. Run away!");
 				menuInputInt = input.nextInt();
 					if (menuInputInt == 1) {
-						System.out.println("You have chosen to battle the Grindylow!");
-						System.out.println("Please enter your HP:");
+						System.out.println("Harry has chosen the Black lake and the swarm of Grindylows...");
+						System.out.println("How much health did Harry have?");
 						int hp = input.nextInt();
 							if (hp > grindylow) {
-								System.out.println("You have defeated the Grindylow!");
+								System.out.println("Relashio!");
+								System.out.println("Harry forced the Grindylows to release him!");
+								hp = hp - grindylow;
+								System.out.println("Harry survives the second task with " + hp + " health remaining.");
 							} else {
-								System.out.println("You have been defeated by the Grindylow!");
+								System.out.println("Harry is drowned and dragged to the depths of the Blake Lake....");
+								System.out.println("This memory is quite different...");
 							}
 					} else if (menuInputInt == 2) {
-						System.out.println("You have chosen to battle the Troll!");
-						System.out.println("Please enter your HP:");
+						System.out.println("Harry remembers the Troll roaming the halls...");
+						System.out.println("How much health did Harry have?");
 						int hp = input.nextInt();
 							if (hp > troll) {
-								System.out.println("You have defeated the Troll!");
+								hp = hp - troll;
+								System.out.println("Wingardium Leviosa!");
+								System.out.println("With Hermoine's help, Harry Potter survives his encounter with the troll with " + hp + " health left");
 							} else {
-								System.out.println("You have been defeated by the Troll!");
+								System.out.println("Harry has been flattened by the Troll!");
+								System.out.println("You've ended a franchise.");
 							}
 					} else if (menuInputInt == 3) {
-						System.out.println("You have chosen to battle the Basilisk!");
-						System.out.println("Please enter your HP:");
+						System.out.println("The first time meeting Tom Riddle and the Basilisk...");
+						System.out.println("How much health did Harry have left after a deathly game of Chess?");
 						int hp = input.nextInt();
 							if (hp > basilisk) {
-								System.out.println("You have defeated the Basilisk!");
+								hp = hp - basilisk;
+								System.out.println("Harry pluges the Sword of Grifindore in the Basiliks mouth!");
+								System.out.println("Harry survives with " + hp + " health and saves Ginnie.");
 							} else {
-								System.out.println("You have been defeated by the Basilisk!");
+								System.out.println("Voldemort is revived, the Death Eaters prevailed.");
 							}
 					} else if (menuInputInt == 4) {
-						System.out.println("You have chosen to battle the Dementor!");
-						System.out.println("To defeat the Dementor, you must have a Patronus charm.");
-						System.out.println("Do you know the Patronus charm? (Y/N)");
+						System.out.println("The Dementors swarm Harry and Sirius at the lake!");
+						System.out.println("Is Harry's Patronus Charm strong enough? (Y/N)");
 						String patronus = input.next();
 							if (patronus.equalsIgnoreCase("Y")) {
-								System.out.println("You have the Patronus charm!");
-								System.out.println("Please enter your HP:");
+								System.out.println("How much strenght does Harry have left?");
 								int hp = input.nextInt();
 									if (hp > dementor) {
-										System.out.println("You have defeated the Dementor!");
+										hp = hp - dementor;
+										System.out.println("Future Harry Potter has saved Harry Potter!?");
+										System.out.println("Which Harry Potter has " + hp + " health left?");
 									} else {
-										System.out.println("You have been defeated by the Dementor!");
+										System.out.println("The Dementor stole a kiss...");
 									}
 							} else {
-								System.out.println("You do not have the Patronus charm!");
-								System.out.println("You have been defeated by the Dementor!");
+								System.out.println("Harry's Patronus charm is too weak against all the Dementors!");
+								System.out.println("Sirius and Harry lose their souls...!");
 							}
 					} else if (menuInputInt == 0) {
 						System.out.println("Thank you for playing!");
