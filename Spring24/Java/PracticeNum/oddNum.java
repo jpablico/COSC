ed2a44a5cb0a672bc21f1e1ef0584c48ff1d4fc9
Josh Pablico <jbpablico27@gmail.com>
2024-02-06 12:44:51 -0600

    SMT

---
+++ Spring24/Java/PracticeNum/oddNum.java
@@ -0,0 +1,13 @@
+package PracticeNum;
+
+public class oddNum {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		for(int x = 1; x <= 50; x += 2) {
+			sum += x; 
+		}
+		System.out.print(sum);
+	}
+	
+}
