1eb32393429a625bd35de398deb5bdfd4391ebf1
Josh Pablico <jbpablico27@gmail.com>
2024-02-13 10:45:33 -0600

    JS

--- Spring24/Java/Chapter5/Methods.java
+++ Spring24/Java/Chapter5/Methods.java
@@ -8,10 +8,21 @@ public class Methods {
 		System.out.println("Please enter a number to triple: ");
 		int num = kbd.nextInt();
 		triple(num);
+
+		System.out.println("Validating if the value is within range of 100 and 0: " + isValid(num));
+
 	}
 
 	public static void triple(int num) {
 		int tripled = num * 3;
 		System.out.println("The value is tripled to " + tripled);
+	}
+
+	public static boolean isValid(int value) {
+		if(value < 100 && value > 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
