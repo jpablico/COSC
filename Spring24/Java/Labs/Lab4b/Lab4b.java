fa29af4adfc319f5fb3f7d2f8ed9fc73a22f9c54
Josh Pablico <jbpablico27@gmail.com>
2024-02-15 10:40:37 -0600

    Needs more work

--- Spring24/Java/Labs/Lab4b/Lab4b.java
+++ Spring24/Java/Labs/Lab4b/Lab4b.java
@@ -4,12 +4,44 @@
  * COSC-1437.200
  */
 
- import java.util.Scanner;
- import java.io.*;
+public class Lab4b {
 
- public class Lab4b {
- 
 	public static void main(String[] args) {
+		int gameStart = 0;
+		gamePlay(gameStart);
+	}
+
+	public static int gamePlay(int gameStart) {
+		if (gameStart == 0) {
+			System.out.println("The Weasley brothers hand you some floo powder and quickly push you into the fireplace");
+			System.out.println("\"You'll need get to Ollivanders Wand emporium in Hogsmeade to get that want fixed...\"");
+			System.out.println("\"Hogsmeade clearly as you drop the floo powder...\"");
+			return gameStart = 1;
+		}
+		return gameStart; // Add a return statement
+	}
+}
+	
+	}
 		
+	// Method to randomize spell cast
+	public static String spellCast(int randomNumber) {
+		String Alohomora = "Alohomora";
+		String Immobulus = "Immobulus";
+		String Wingardium = "Wingardium Leviosa";
+
+		if(randomNumber <= 33) {
+			return Alohomora;
+		} else if(randomNumber <= 66) {
+			return Immobulus;
+		} else {
+			return Wingardium;
+		}
+	}
+
+	// Method to generate a random number
+	public static int randomNumber() {
+		int rndNum = (int)(Math.random() * 100) + 1;
+		return rndNum;
 	}
  }
