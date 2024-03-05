916d0580f91e47e29c0ee6eafd5717c982daca95
Josh Pablico <jbpablico27@gmail.com>
2024-03-05 10:28:25 -0600

    Java

--- Spring24/Java/Chapter6/Rectangle/src/RectangleTester.java
+++ Spring24/Java/Chapter6/Rectangle/src/RectangleTester.java
@@ -2,5 +2,14 @@ public class RectangleTester {
 	
 	public static void main(String[] args) {
 		Rectangle r = new Rectangle();
+
+		System.out.println(r.getLength());
+		r.setLength(8);
+		r.setWidth(5);
+		System.out.println(r.getLength());
+		System.out.println(r.getWidth());
+
+		System.out.println(r.getArea());
 	}
+
 }
