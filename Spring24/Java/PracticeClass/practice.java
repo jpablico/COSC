e7f68537db5eab122b9c583b9b94b0ce7d96e059
Josh Pablico <jbpablico27@gmail.com>
2024-03-07 08:44:56 -0600

    Java

--- Spring24/Java/PracticeClass/practice.java
+++ Spring24/Java/PracticeClass/practice.java
@@ -1,24 +1,46 @@
 package PracticeClass;
 
-import java.util.Random;
+public class Spaceship {
+    private String name;
+    private int speed;
 
-public class practice {
-	
-	public static void main(String[] args) {
-		
-		int [][] array = new int[6][4];
-		
-		for(int x = 0; x<array.length; x++) {
-			for(int i = 0; i<array[x].length; i++){
-				array[x][i] = randomNumber();
-			}
+    public Spaceship(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+    }
 
-		}
-		System.out.println(array);
-	}
+    public String getName() {
+        return name;
+    }
 
-	public static int randomNumber() {
-		int rndNum = (int)(Math.random() * 100) + 1;
-		return rndNum;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void accelerate(int increase) {
+        speed += increase;
+        System.out.println(name + " accelerating to " + speed + " km/h");
+    }
+
+    public void decelerate(int decrease) {
+        speed = Math.max(0, speed - decrease);
+        System.out.println(name + " decelerating to " + speed + " km/h");
+    }
+
+    public static void main(String[] args) {
+        Spaceship mySpaceship = new Spaceship("Galactic Voyager", 5000);
+        System.out.println("Spaceship Name: " + mySpaceship.getName());
+        System.out.println("Initial Speed: " + mySpaceship.getSpeed() + " km/h");
+
+        mySpaceship.accelerate(1000);
+        mySpaceship.decelerate(2000);
+    }
 }
