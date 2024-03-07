d12c181ec78d816d5322e6474b73fec8e9a8e2b1
Josh Pablico <jbpablico27@gmail.com>
2024-03-07 10:17:57 -0600

    Java

--- Spring24/Java/Chapter6/Rectangle/src/Rectangle.java
+++ Spring24/Java/Chapter6/Rectangle/src/Rectangle.java
@@ -3,6 +3,16 @@ public class Rectangle {
     private double length;
 	private double width;
 
+	public Rectangle(double len, double wid) {
+		length = len;
+		width = wid;
+	}
+
+	public Rectangle() {
+		length= 1;
+		width= 1;
+	}
+
 	public void setLength(double len) {
 		length = len;
 	}
---
+++ Spring24/Java/Chapter6/Rectangle/src/Rooms.java
@@ -0,0 +1,14 @@
+public class Rooms {
+	
+	public static void main(String[] args) {
+		Rectangle kitchen = new Rectangle(12,15);
+		Rectangle bedroom = new Rectangle(3, 4);
+
+		kitchen.setLength(12);
+		kitchen.setWidth(15);
+		bedroom.setLength(10);
+		bedroom.setWidth(11);
+		
+	}
+	
+}
