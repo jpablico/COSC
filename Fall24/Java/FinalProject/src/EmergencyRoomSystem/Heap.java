81d69fe600a695de54dcbb734543b0b1b652d703
Josh Pablico <jbpablico27@gmail.com>
2024-12-05 13:53:44 -0600

    add function created. Modualized code, created a swap

--- Fall24/Java/FinalProject/src/EmergencyRoomSystem/Heap.java
+++ Fall24/Java/FinalProject/src/EmergencyRoomSystem/Heap.java
@@ -13,11 +13,30 @@ public class Heap<T> implements PriorityQueue<T> {
 
   @Override
   public void add(T element) {
+    heap.add(element);
+    int index = heap.size() - 1;
 
+    while (index > 0) {
+      int parentIndex = (index - 1) / 2;
+      if (comparator.compare(heap.get(index), heap.get(parentIndex)) >= 0) {
+        break;
+      }
+      swap(index, parentIndex);
+      index = parentIndex;
+    }
   }
 
   @Override
   public T remove() {
-    return null;
+    if (heap.isEmpty()) {
+      return null;
+    }
+    
+  }
+
+  private void swap(int index1, int index2) {
+    T temp = heap.get(index1);
+    heap.set(index1, heap.get(index2));
+    heap.set(index2, temp);
   }
 }
