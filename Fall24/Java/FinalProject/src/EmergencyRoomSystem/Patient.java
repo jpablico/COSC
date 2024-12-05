add90d2e3985873aaf4c19e6e75217178e52ba89
Josh Pablico <jbpablico27@gmail.com>
2024-12-05 15:41:21 -0600

    Worked on patient class. Need to work on a comparator for patients next

--- Fall24/Java/FinalProject/src/EmergencyRoomSystem/Patient.java
+++ Fall24/Java/FinalProject/src/EmergencyRoomSystem/Patient.java
@@ -3,7 +3,43 @@ import java.time.LocalTime;
 
 public class Patient {
   private final String name;
-  private final LocalDate dob
-  private final Severity severity;
-  private final LocalDate dateOfBirth;
+  private final LocalDate dob;
+  private Severity severity;
+  private LocalTime arrivalTime;
+
+  public Patient(String name, LocalDate dob, Severity severity) {
+    this.name = name;
+    this.dob = dob;
+    this.severity = severity;
+    this.arrivalTime = LocalTime.now();
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public LocalDate getDob() {
+    return dob;
+  }
+
+  public Severity getSeverity() {
+    return severity;
+  }
+
+  public void setSeverity(Severity severity) {
+    this.severity = severity;
+  }
+
+  public LocalTime getArrivalTime() {
+    return arrivalTime;
+  }
+
+  public void setArrivalTime(LocalTime arrivalTime) {
+    this.arrivalTime = arrivalTime;
+  }
+
+  @Override
+  public String toString() {
+    return name + " (DOB: " + dob + ", Severity: " + severity + ", Arrival Time: " + arrivalTime + ")";
+  }
 }
