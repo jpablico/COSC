f21925d92cf415fcde1c42379e75a69b9621bb85
Josh Pablico <jbpablico27@gmail.com>
2024-12-07 21:03:34 -0600

    Fixed main for ER demo.

--- Fall24/Java/FinalProject/src/EmergencyRoomSystem/EmergencyRoomDemo.java
+++ Fall24/Java/FinalProject/src/EmergencyRoomSystem/EmergencyRoomDemo.java
@@ -1,15 +1,20 @@
 import java.time.LocalDate;
+import java.util.concurrent.TimeUnit;
 
-public class EmergencyRoomTest {
-    public static void main(String[] args) {
+public class EmergencyRoomDemo {
+    public static void main(String[] args) throws InterruptedException {
         PatientComparator comparator = new PatientComparator();
         PriorityQueue<Patient> heap = new Heap<>(comparator);
         EmergencyRoom eRoom = new EmergencyRoom(heap);
 
         eRoom.checkIn(new Patient("Mirabella Jones", LocalDate.of(1973, 7, 11), Severity.SEVEN));
+        TimeUnit.MILLISECONDS.sleep(10);
         eRoom.checkIn(new Patient("Ruth Mendez", LocalDate.of(1965, 1, 22), Severity.SEVEN));
+        TimeUnit.MILLISECONDS.sleep(10);
         eRoom.checkIn(new Patient("Melvin Ingram", LocalDate.of(1965, 1, 22), Severity.SEVEN));
+        TimeUnit.MILLISECONDS.sleep(10);
         eRoom.checkIn(new Patient("Tara Silva", LocalDate.of(1975, 5, 8), Severity.EIGHT));
+        TimeUnit.MILLISECONDS.sleep(10);
         eRoom.checkIn(new Patient("Jeff Barnes", LocalDate.of(1968, 12, 19), Severity.EIGHT));
 
         System.out.println("\nAdmitting patients:");
